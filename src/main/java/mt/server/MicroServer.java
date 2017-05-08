a235841e344d601f5bf6cbaebc4420ce6934a943
Goncalo Ribeiro <gjqar@iscte-iul.pt>
2017-05-08 14:10:15 +0100

    EU

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -218,6 +218,7 @@ public class MicroServer implements MicroTraderServer {
 		LOGGER.log(Level.INFO, "Processing new order...");
 
 		Order o = msg.getOrder();
+		//fazer ficheiro xml!
 		
 		// save the order on map
 		saveOrder(o);
